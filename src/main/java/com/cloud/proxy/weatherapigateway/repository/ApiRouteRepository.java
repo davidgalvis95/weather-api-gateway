7916e2fa975ff5ab5aa89bac18c8ca5d548878e4
David Galvis <>
2021-11-06 13:39:28 -0500

    Creating testCreateNewFakeRouteAndCallToFakeService gateway test

--- src/main/java/com/cloud/proxy/weatherapigateway/repository/ApiRouteRepository.java
+++ src/main/java/com/cloud/proxy/weatherapigateway/repository/ApiRouteRepository.java
@@ -2,6 +2,7 @@ package com.cloud.proxy.weatherapigateway.repository;
 
 import com.cloud.proxy.weatherapigateway.model.ApiRoute;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
--- src/test/java/integration/com/cloud/proxy/weatherapigateway/api/WeatherApiGatewayIntgTest.java
+++ src/test/java/integration/com/cloud/proxy/weatherapigateway/api/WeatherApiGatewayIntgTest.java
@@ -1,6 +1,9 @@
 package com.cloud.proxy.weatherapigateway.api;
 
 import com.cloud.proxy.weatherapigateway.config.CustomTestDBContainer;
+import com.cloud.proxy.weatherapigateway.model.ApiRoute;
+import com.cloud.proxy.weatherapigateway.model.CreateOrUpdateApiRouteRequest;
+import com.cloud.proxy.weatherapigateway.repository.ApiRouteRepository;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 import org.junit.ClassRule;
@@ -10,14 +13,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
+import static com.cloud.proxy.weatherapigateway.model.WeatherGatewayApiPath.INTERNAL_API_ROUTES;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.HttpHeaders.ACCEPT;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @ActiveProfiles("test")
 @Testcontainers
@@ -25,16 +37,23 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WeatherApiGatewayIntgTest {
 
-    @Autowired
-    private WebTestClient webTestClient;
-
-    @ClassRule
-    public static PostgreSQLContainer<CustomTestDBContainer> container = CustomTestDBContainer.getInstance();
-
     private static WireMockServer wireMockServer;
+
+    public static final String FAKE_SERVICE_PATH = "/fake/response";
+
+    public static final String FAKE_SERVICE_RESPONSE_PAYLOAD = "{\n\"fakePayload\":{\n\"fakeProp1\":1,\n\"fakeProp2\":2\n}\n}";
 
     @Value("${fakeService.uri}")
     private String urii;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Autowired
+    private ApiRouteRepository apiRouteRepository;
+
+    @ClassRule
+    public static PostgreSQLContainer<CustomTestDBContainer> container = CustomTestDBContainer.getInstance();
 
 
     @BeforeAll
@@ -43,23 +62,51 @@ public class WeatherApiGatewayIntgTest {
         wireMockServer.start();
         System.setProperty("FAKE_SERVICE_URL", wireMockServer.baseUrl());
         container.start();
+        generateStubs();
     }
 
     @Test
-    void testCallToLocationService(){
-        stubFor(get(urlEqualTo("/get"))
-                .willReturn(aResponse()
-                        .withBody("{\"headers\":{\"Hello\":\"World\"}}")
-                        .withHeader("Content-Type", "application/json")));
-        stubFor(get(urlEqualTo("/delay/3"))
-                .willReturn(aResponse()
-                        .withBody("no fallback")
-                        .withFixedDelay(3000)));
+    void testCreateNewFakeRouteAndCallToFakeService(){
 
-        var x = wireMockServer.baseUrl();
-        var y = urii;
-        System.out.println(wireMockServer.baseUrl());
-        assertTrue(wireMockServer.isRunning());
+        //Create a new ApiRoute
+        final CreateOrUpdateApiRouteRequest createRequest = CreateOrUpdateApiRouteRequest.builder()
+                .path(FAKE_SERVICE_PATH)
+                .method(HttpMethod.GET.name())
+                .uri(wireMockServer.baseUrl())
+                .build();
+
+        final FluxExchangeResult<ApiRoute> reactiveResponse = webTestClient.post()
+                .uri(INTERNAL_API_ROUTES)
+                .header(ACCEPT, APPLICATION_JSON_VALUE)
+                .body(Mono.just(createRequest), CreateOrUpdateApiRouteRequest.class)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .returnResult(ApiRoute.class);
+
+        final ApiRoute createdRoute = reactiveResponse.getResponseBody().share().blockFirst();
+
+        //Query the repository and get the ApiRoute by id and compare against the created one
+        assertNotNull(createdRoute);
+        assertNotNull(Optional.of(createdRoute).map(ApiRoute::getId).orElse(null));
+
+        final ApiRoute currentRoute = apiRouteRepository.findById(createdRoute.getId()).block();
+
+        assertNotNull(currentRoute);
+        assertEquals(createdRoute.getMethod(), currentRoute.getMethod());
+        assertEquals(createdRoute.getPath(), currentRoute.getPath());
+        assertEquals(createdRoute.getUri(), currentRoute.getUri());
+
+
+        //Hit the gateway to get the fake service's response of the created payload
+        webTestClient.get()
+                .uri(FAKE_SERVICE_PATH)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody()
+                .jsonPath("$.fakePayload.fakeProp1").isEqualTo(1)
+                .jsonPath("$.fakePayload.fakeProp2").isEqualTo(2);
     }
 
 
@@ -67,5 +114,17 @@ public class WeatherApiGatewayIntgTest {
     static void tearDown() {
         wireMockServer.stop();
         container.stop();
+    }
+
+    private static void generateStubs(){
+        wireMockServer.stubFor( get(urlEqualTo(FAKE_SERVICE_PATH))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withBody(FAKE_SERVICE_RESPONSE_PAYLOAD)
+                        .withHeader(ACCEPT, APPLICATION_JSON_VALUE)));
+//        stubFor(get(urlEqualTo("/delay/3"))
+//                .willReturn(aResponse()
+//                        .withBody("no fallback")
+//                        .withFixedDelay(3000)));
     }
 }
